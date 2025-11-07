defbdb14a71a224b363b2219f3fbb33dc497d9b2
shivamsingh8767 <dynamoshiv@gmail.com>
2025-11-07 10:11:51 +0530

    Add Armstrong number calculation in Armstrong.c
    Implement Armstrong number check and print for numbers 1 to 10000.

---
+++ Armstrong.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int armstrong(int n)    {
+    int temp = n;
+    int len, result = 0;
+    for(len = 0; temp; len++, temp/=10);
+    temp = n;
+    
+    while(temp) {
+        int lastDigit = temp % 10, pow = 1;
+        temp /= 10;
+
+        for(int x = 1; x <= len; x++)
+            pow = pow * lastDigit;
+        
+        result += pow;
+    }
+
+    return (n == result);
+}
+
+
+
+int main()
+{
+    for(int x = 1; x <= 10000; x++) 
+        if(armstrong(x)) 
+            printf("%d ",x);
+       
+    printf("\n");
+    return 0; 
+}
